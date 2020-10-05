f2a279786afc8392942567340ae0ce03b15febd5
David Medina <davidamedina619@gmail.com>
2020-10-05 18:02:39 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,1 +1,8 @@
 # Write your code here
+
+magic_exit_number = 1
+count = 10
+while count == 1< 10 && count != magic_exit_number do
+  puts "I am the #{count}, I love to count!" # Work
+  count -= 1
+end
